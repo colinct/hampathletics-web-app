0345286b3032c042838db610242e6dec42f026ed
colinct <colin@tyrannosaurustech.com>
2023-01-31 16:30:18 -0500

    chore: commit to update repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import { Button } from "../src/App/atoms/Button";
+import { Header } from "./App/molecules/Header";
 
 function App() {
   return (
     <div>
+      <Header />
       <Button />
     </div>
   );
---
+++ src/App/molecules/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+export const Header = () => {
+  const StyledHeader = styled.header`
+  height: 100px;
+  width: maxvw;
+  background-color: red;
+
+  `;
+
+  return <StyledHeader>Header</StyledHeader>;
+};
