f840f300d62566dce52123ccfd8532d3b365671a
colinct <colin@tyrannosaurustech.com>
2023-01-31 16:45:28 -0500

    wip: build header bar

--- src/App/molecules/Header.tsx
+++ src/App/molecules/Header.tsx
@@ -3,11 +3,28 @@ import styled from "styled-components";
 
 export const Header = () => {
   const StyledHeader = styled.header`
-  height: 100px;
-  width: maxvw;
-  background-color: red;
-
+    display: flex;
+    height: 100px;
+    width: 100%;
+    background-color: red;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 1rem;
   `;
+  const StyledHeaderTabs = styled.div`
+    display: flex;  
+    gap: 1rem;
+    padding-right: 1rem;
+    `;
 
-  return <StyledHeader>Header</StyledHeader>;
+  return (
+    <StyledHeader>
+      <div>HampAthletics</div>
+      <StyledHeaderTabs>
+        <div>Workout Plans</div>
+        <div>Contact Info</div>
+        <div>Schedule Consulation</div>
+      </StyledHeaderTabs>
+    </StyledHeader>
+  );
 };
