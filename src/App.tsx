008384369cb94a43b80ccfae0f06e3da0a3952bc
twicegravity <colinoftompkins@gmail.com>
2023-01-31 16:04:15 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import { Button } from "../src/App/atoms/Button";
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
