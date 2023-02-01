77a30272d09387d2c6575c7287a86ccdf323321c
colinct <colin@tyrannosaurustech.com>
2023-01-31 21:43:21 -0500

    wip: more header stylings and add tailwind to project

--- src/App/molecules/Header.tsx
+++ src/App/molecules/Header.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import styled from "styled-components";
+import hamplogo from "../assets/hamplogo.png";
 
 export const Header = () => {
   const StyledHeader = styled.header`
     display: flex;
     height: 100px;
     width: 100%;
-    background-color: red;
+    background: linear-gradient(90deg, rgba(214,212,236,1) 0%, rgba(115,115,255,1) 35%, rgba(0,78,255,1) 100%);
     align-items: center;
     justify-content: space-between;
     padding-left: 1rem;
   `;
   const StyledHeaderTabs = styled.div`
-    display: flex;  
+    display: flex;
     gap: 1rem;
     padding-right: 1rem;
-    `;
+  `;
+
+  const StyledHeaderLogo = styled.div`
+    display: flex;
+    align-items: center;
+  `;
 
   return (
     <StyledHeader>
-      <div>HampAthletics</div>
+      <StyledHeaderLogo>
+        <img
+          src={hamplogo}
+          alt="Hamp Athletics Logo"
+          style={{ height: "100px" }}
+        />
+        <div>HampAthletics</div>
+      </StyledHeaderLogo>
       <StyledHeaderTabs>
         <div>Workout Plans</div>
         <div>Contact Info</div>
